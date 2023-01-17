7cab7d37d378ecf8dd09a5aeea078881579f9db1
Yogesh Sathe <ysathe991@gmail.com>
2023-01-17 20:14:03 +0530

    day1 all issue solved

---
+++ scripts/girl.js
@@ -0,0 +1,52 @@
+
+fetch("../Database/girldata.json")
+.then((res)=>{
+    return res.json();
+})
+.then((data)=>{
+    appendData(data)
+})
+.catch((err)=>{
+    return err
+})
+
+let appendData = (data) => {
+    console.log(data)
+    let container = document.getElementById("product-container");
+    container.innerHTML = "";
+    data.forEach((el) => {
+        let card = document.createElement("div");
+        card.setAttribute("id", "card");
+
+        let img = document.createElement("img");
+        img.src = el.image;
+
+        let name = document.createElement("h3");
+        name.textContent = el.title;
+
+        let des = document.createElement("p");
+        des.textContent = el.description;
+
+        let price = document.createElement("p");
+        price.textContent = "₹ "+el.price+".00";
+
+        let btn = document.createElement("button");
+        btn.textContent = "Add to Cart";
+        btn.setAttribute("id", "btn");
+        btn.addEventListener("click", () => {
+            Addtocart(el);
+            console.log("button working perfectly")
+        })
+
+        card.append(img, name, des, price, btn)
+        container.append(card)
+
+    })
+}
+
+function Addtocart(ele) {
+    let arr = JSON.parse(localStorage.getItem('add-to-cart')) || [];
+    arr.push(ele)
+    localStorage.setItem('add-to-cart', JSON.stringify(arr))
+
+}
