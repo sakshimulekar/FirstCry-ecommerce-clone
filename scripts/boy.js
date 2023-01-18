6d9315e84cf257260eb5724b0dd616157cd85f2d
Yogesh Sathe <ysathe991@gmail.com>
2023-01-18 21:15:04 +0530

    day2 all tasks completed

--- scripts/boy.js
+++ scripts/boy.js
@@ -1,52 +1,341 @@
+let mydata = [
+  {
+    "id": 1,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12704810a.webp",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 250
+  },
 
-fetch("../Database/boydata.json")
-.then((res)=>{
-    return res.json();
-})
-.then((data)=>{
-    appendData(data)
-})
-.catch((err)=>{
-    return err
-})
+  {
+    "id": 2,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12652276a.webp",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 350
+  },
 
-let appendData = (data) => {
-    console.log(data)
-    let container = document.getElementById("product-container");
-    container.innerHTML = "";
-    data.forEach((el) => {
-        let card = document.createElement("div");
-        card.setAttribute("id", "card");
+  {
+    "id": 3,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11425519a.webp",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 450
+  },
 
-        let img = document.createElement("img");
-        img.src = el.image;
+  {
+    "id": 4,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12141710a.webp",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 250
+  },
 
-        let name = document.createElement("h3");
-        name.textContent = el.title;
+  {
+    "id": 5,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12223087a.webp",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 250
+  },
 
-        let des = document.createElement("p");
-        des.textContent = el.description;
+  {
+    "id": 6,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12627589a.webp",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 350
+  },
 
-        let price = document.createElement("p");
-        price.textContent = "₹ "+el.price+".00";
+  {
+    "id": 7,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12627464a.webp",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 350
+  },
 
-        let btn = document.createElement("button");
-        btn.textContent = "Add to Cart";
-        btn.setAttribute("id", "btn");
-        btn.addEventListener("click", () => {
-            Addtocart(el);
-            console.log("button working perfectly")
-        })
+  {
+    "id": 8,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11683453a.webp",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 250
+  },
 
-        card.append(img, name, des, price, btn)
-        container.append(card)
+  {
+    "id": 9,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11568732b.jpg",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 450
+  },
+
+  {
+    "id": 10,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12442602b.jpg",
+    "title": "boy T-shirt",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 450
+  },
+
+  {
+    "id": 11,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11568732b.jpg",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 1472
+  },
+
+  {
+    "id": 12,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12248274a.webp",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 1690
+  },
+
+  {
+    "id": 13,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12602537a.webp",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 1800
+  },
+
+  {
+    "id": 14,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11712081a.webp",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 2279
+  },
+
+  {
+    "id": 15,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12490417a.webp",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 1300
+  },
+
+  {
+    "id": 16,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11515574a.webp",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 1500
+  },
+
+  {
+    "id": 17,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12688812a.webp",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 2500
+  },
+
+  {
+    "id": 18,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11228570b.jpg",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 1970
+  },
+
+  {
+    "id": 19,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12688777a.webp",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 1799
+  },
+
+  {
+    "id": 20,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11214403a.webp",
+    "title": "ethnic wear",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 1999
+  },
+
+  {
+    "id": 21,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12143032a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 512
+  },
+
+  {
+    "id": 22,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11425440a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 710
+  },
+
+  {
+    "id": 23,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/10851397a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 710
+  },
+
+  {
+    "id": 24,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11748779a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 591
+  },
+
+  {
+    "id": 25,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12254761a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 680
+  },
+
+  {
+    "id": 26,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11823502a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 670
+  },
+
+  {
+    "id": 27,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12254922a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 715
+  },
+
+  {
+    "id": 28,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11422158a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 880
+  },
+
+  {
+    "id": 29,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/10734523a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 834
+  },
+
+  {
+    "id": 30,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12738988a.webp",
+    "title": "jeans & trousers",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 999
+  }
+]
+
+let defaultArr = [...mydata]
+// fetch("../Database/boydata.json")
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data)=>{
+//     appendData(data)
+// })
+// .catch((err)=>{
+//     return err
+// })
+
+let appendData = (mydata) => {
+  // console.log(data)
+  let container = document.getElementById("product-container");
+  container.innerHTML = "";
+  mydata.forEach((el) => {
+    let card = document.createElement("div");
+    card.setAttribute("id", "card");
+
+    let img = document.createElement("img");
+    img.src = el.image;
+
+    let name = document.createElement("h3");
+    name.textContent = el.title;
+
+    let des = document.createElement("p");
+    des.textContent = el.description;
+
+    let price = document.createElement("p");
+    price.textContent = "₹ " + el.price + ".00";
+
+    let btn = document.createElement("button");
+    btn.textContent = "Add to Cart";
+    btn.setAttribute("id", "btn");
+    btn.addEventListener("click", () => {
+      Addtocart(el);
 
     })
-}
 
-function Addtocart(ele) {
-    let arr = JSON.parse(localStorage.getItem('add-to-cart')) || [];
-    arr.push(ele)
+    card.append(img, name, des, price, btn)
+    container.append(card)
+
+  })
+}
+appendData(mydata);
+
+function Addtocart(el) {
+  let arr = JSON.parse(localStorage.getItem('add-to-cart')) || [];
+  if (arr === null) { arr = []; }
+
+  let present = false;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].id === el.id) {
+      present = true;
+      break;
+    }
+
+  }
+  if (present == true) {
+    swal("Already in Cart", "Product Already Present in Cart!", "info");
+
+  } else {
+    arr.push(el)
     localStorage.setItem('add-to-cart', JSON.stringify(arr))
+    swal("Success", "Your Product Added to Cart!", "success");
+    // swal.style.backgroundColor = "green"
+    
+
+  }
 
 }
+
+
+let sorted = document.querySelector("#sort");
+sorted.addEventListener("change", function (event) {
+
+  let val = document.querySelector("#sort").value;
+  console.log(val)
+
+  if (val == "LTH") {
+    let data1 = mydata.sort(function (a, b) {
+      return a.price - b.price;
+
+    })
+    appendData(data1)
+
+  } else if (val == "HTL") {
+    let sorteddata = mydata.sort(function (a, b) {
+      return b.price - a.price;
+
+    })
+    appendData(sorteddata)
+
+  } else if(val == "default") {
+    appendData(defaultArr);
+    console.log(defaultArr)
+  }
+})
--- scripts/toddler.js
+++ scripts/toddler.js
@@ -1,56 +1,199 @@
-console.log("child script working")
-var data;
+let mydata = [
+  {
+    "id": 1,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/12612293a.webp",
+    "title": "toddler",
+    "category": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 250
+  },
 
-fetch("/Database/toddler.json")
-.then((res)=>{
-    return res.json();
-})
-.then((data)=>{
-    appendData(data)
-    console.log(data)
-})
-.catch((err)=>{
-    return err
-})
+  {
+    "id": 2,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11909989a.webp",
+    "title": "toddler",
+    "category": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 631
+  },
 
-let appendData = (data) => {
-    let container = document.getElementById("product-container");
-    container.innerHTML = "";
-    data.forEach((el) => {
-        let card = document.createElement("div");
-        card.setAttribute("id", "card");
+  {
+    "id": 3,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/300x364/9293740a.webp",
+    "category": "boy",
+    "title": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 694
+  },
 
-        let img = document.createElement("img");
-        img.src = el.image;
+  {
+    "id": 4,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/219x265/3218784a.webp",
+    "category": "boy",
+    "title": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 631
+  },
 
-        let name = document.createElement("h3");
-        name.textContent = el.title;
+  {
+    "id": 5,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/219x265/11615387a.webp",
+    "category": "boy",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "title": "child",
+    "price": 672
+  },
 
-        let des = document.createElement("p");
-        des.textContent = el.description;
+  {
+    "id": 6,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/219x265/11725480a.webp",
+    "category": "boy",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "title": "child",
+    "price": 321
+  },
 
-        let price = document.createElement("p");
-        price.textContent = "₹ "+el.price+".00";
+  {
+    "id": 7,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/219x265/11734360a.webp",
+    "category": "boy",
+    "title": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 299
+  },
 
-        let btn = document.createElement("button");
-        btn.textContent = "Add to Cart";
-        btn.setAttribute("id", "btn");
-        btn.addEventListener("click", () => {
-            Addtocart(el);
-            console.log("button working perfectly")
-        })
+  {
+    "id": 8,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11744443a.webp",
+    "category": "boy",
+    "title": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 220
+  },
 
-        
+  {
+    "id": 9,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/219x265/12301457a.webp",
+    "category": "boy",
+    "title": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 846
+  },
 
-        card.append(img, name, des, price, btn)
-        container.append(card)
+  {
+    "id": 10,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/219x265/12717234a.webp",
+    "category": "boy",
+    "title": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 923
+  },
+
+  {
+    "id": 11,
+    "image": "https://cdn.fcglcdn.com/brainbees/images/products/219x265/12324913a.webp",
+    "category": "boy",
+    "title": "child",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "price": 522
+  }
+]
+// fetch("/Database/toddler.json")
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data)=>{
+//     appendData(data)
+//     console.log(data)
+// })
+// .catch((err)=>{
+//     return err
+// })
+
+let appendData = (mydata) => {
+  let container = document.getElementById("product-container");
+  container.innerHTML = "";
+  mydata.forEach((el) => {
+    let card = document.createElement("div");
+    card.setAttribute("id", "card");
+
+    let img = document.createElement("img");
+    img.src = el.image;
+
+    let name = document.createElement("h3");
+    name.textContent = el.title;
+
+    let des = document.createElement("p");
+    des.textContent = el.description;
+
+    let price = document.createElement("p");
+    price.textContent = "₹ " + el.price + ".00";
+
+    let btn = document.createElement("button");
+    btn.textContent = "Add to Cart";
+    btn.setAttribute("id", "btn");
+    btn.addEventListener("click", () => {
+      Addtocart(el);
+      console.log("button working perfectly")
+    })
+
+
+
+    card.append(img, name, des, price, btn)
+    container.append(card)
+
+  })
+}
+appendData(mydata);
+
+function Addtocart(el) {
+  let arr = JSON.parse(localStorage.getItem('add-to-cart')) || [];
+  if (arr === null) { arr = []; }
+
+  let present = false;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].id === el.id) {
+      present = true;
+      break;
+    }
+
+  }
+  if (present == true) {
+    swal("Already in Cart", "Product Already Present in Cart!", "error");
+
+  } else {
+    arr.push(el)
+    localStorage.setItem('add-to-cart', JSON.stringify(arr))
+    swal("Success", "Your Product Added to Cart!", "success");
+
+
+  }
+
+}
+
+
+let sorted = document.querySelector("#sort");
+
+sorted.addEventListener("change", function (event) {
+
+  let val = document.querySelector("#sort").value;
+  console.log(val)
+
+  if (val == "LTH") {
+    let data1 = mydata.sort(function (a, b) {
+      return a.price - b.price;
 
     })
-}
+    appendData(data1)
 
-function Addtocart(ele) {
-    let arr = JSON.parse(localStorage.getItem('add-to-cart')) || [];
-    arr.push(ele)
-    localStorage.setItem('add-to-cart', JSON.stringify(arr))
+  } else if (val == "HTL") {
+    let sorteddata = mydata.sort(function (a, b) {
+      return b.price - a.price;
 
-}
+    })
+    appendData(sorteddata)
+
+  } else {
+    appendData(mydata);
+  }
+})
