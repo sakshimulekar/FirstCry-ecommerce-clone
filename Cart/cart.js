886393b1ce6696c7871677edd44041fba9a3aa6a
sakshimulekar <sakshimulekar2017@gmail.com>
2023-01-22 21:38:56 +0530

    code correct

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,19 +1,42 @@
 //console.log("hii");
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 
-let getData = JSON.parse(localStorage.getItem("add-to-cart")) || [];
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "View more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "View less"; 
+      moreText.style.display = "inline";
+    }
+  }
 
-let wishData = JSON.parse(localStorage.getItem("wishlist")) || [];
+let getData = JSON.parse(localStorage.getItem("add-to-cart"))||[];
+
+let wishData = JSON.parse(localStorage.getItem("wishlist"))||[];
 
 
-for (let p of getData) {
+for(let p of getData){
     p.Q = 1
 }
-// console.log(getData)
+console.log(getData)
 
 var card = document.getElementById("product-container");
 let heading = document.getElementById("heading")
 
-let sum = document.getElementById("sum")
+let sum = document .getElementById("sum")
 
 fetchData(wishData);
 
@@ -22,12 +45,12 @@ fetchData(getData);
 
 
 
-function fetchData(data) {
+function fetchData(data){
 
     card.innerHTML = null;
-
-    if (getData.length === 0) {
-        alert("Empty Cart")
+    
+    if(getData.length === 0){
+    alert("Empty Cart")
     }
 
     let total = 0;
@@ -53,41 +76,28 @@ function fetchData(data) {
         description.innerText = ele.description;
 
         let prize = document.createElement("h3");
-        prize.innerText = "Price : " + ele.price + ".00 ₹";
-
+        prize.innerText = "Price : "+ele.price+".00 ₹";
+       
         let removeBtn = document.createElement("button");
         removeBtn.innerText = `Remove ~`;
-        removeBtn.setAttribute("id", "btn")
-        removeBtn.addEventListener("click", () => {
+        removeBtn.setAttribute("id","btn")
+        removeBtn.addEventListener("click",()=>{
 
-            data.splice(ele, 1);
+            data.splice(ele,1);
 
-            alert("product remove successfully")
+            swal("Remove Successfully", "Product removed from Cart!", "success");
 
-            localStorage.setItem("add-to-cart", JSON.stringify(data));
+
+            localStorage.setItem("add-to-cart",JSON.stringify(data));
 
             fetchData(data);
             console.log(data);
-
+            
         })
 
         let box = document.createElement("div");
         box.innerHTML = ` <i class="fa-solid fa-trash"></i>`;
 
-        let shortBtn = document.createElement("button");
-        shortBtn.setAttribute("id", "btn1")
-        shortBtn.innerText = "❤️ Move to Shortlist";
-
-        shortBtn.addEventListener("click", () => {
-            data.splice(ele, 1);
-
-            localStorage.setItem("wishlist", JSON.stringify(data));
-
-            fetchData(data);
-            console.log(data);
-        })
-
-        //......quantity part......//
         let sel = document.createElement("div");
         sel.setAttribute("id", "sel");
         sel.innerText = "Quantity : "
@@ -119,14 +129,22 @@ function fetchData(data) {
         select.addEventListener("change", (e) => {
             // let qty = document.getElementById("qty").value;
             let qty = e.target.value;
+            if(qty==1){
+              opt1.innerText = 1;
+            }else if(qty==2){
+              opt2.innerText = 2
+            }else if(qty==3){
+              opt3.innerText = 3
+            }
             console.log(qty)
             incre = +(e.target.value)
+            //select.innerText = qty
             select.innerText = incre
             ele.Q = incre
             localStorage.setItem("add-to-cart", JSON.stringify(data));
             fetchData(data);
         })
-
+       
         //.....Size Selection...//
         let size = document.createElement("div");
         size.setAttribute("id", "size");
@@ -146,25 +164,41 @@ function fetchData(data) {
         let s5 = document.createElement("option");
         s5.innerText = "XXL";
 
-        localStorage.setItem("wishlist", JSON.stringify(data));
+        localStorage.setItem("wishlist",JSON.stringify(data));
 
-        localStorage.setItem("add-to-cart", JSON.stringify(data));
+        localStorage.setItem("add-to-cart",JSON.stringify(data));
 
         div2.append(img)
         removeBtn.append(box);
         sel.append(select);
-        select.append(opt1, opt2, opt3, opt4, opt5);
+        select.append(opt1,opt2,opt3,opt4,opt5);
         size.append(siz)
-        siz.append(s1, s2, s3, s4, s5)
-        div1.append(title, description, prize, sel, size, removeBtn, shortBtn)
-        div.append(div2, div1);
+        siz.append(s1,s2,s3,s4,s5)
+        div1.append(title,description,prize,sel,size,removeBtn)
+        div.append(div2,div1);
         card.append(div);
-
-        total = total + ele.price * ele.Q
-
+     
+        total = total+ele.price*ele.Q;
+       
     })
-
+    
     heading.innerText = total
 
+    var valPro = document.getElementById("val_p");
+    valPro.innerText = ":   "+total+" ₹";
+    let discountamnt = Math.floor(10+Math.random()*10)
+    console.log(1-(discountamnt/100));
+    var Discount = document.getElementById("Dis");
+    Discount.innerHTML = ":   "+Math.floor(Math.random()*10)+"%";
+    //let dis = Discount.value/100;
+
+    var shipping = document.getElementById("ship")
+    shipping.innerText = "Shipping (+)  :    "+"Free"
+
+    var sTotal = document.getElementById("sTotal");
+
+    sTotal.innerText = Math.floor(total*(1-(discountamnt/100)))
+    console.log(Math.floor(total*(1-(discountamnt/100))));
 
 }
+
