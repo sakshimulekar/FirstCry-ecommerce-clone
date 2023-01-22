631619e70a7ab570a00809464f245c651bba9f2a
Yogesh Sathe <ysathe991@gmail.com>
2023-01-22 19:37:56 +0530

    project done

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,11 +1,11 @@
 //console.log("hii");
 
-let getData = JSON.parse(localStorage.getItem("add-to-cart"))||[];
+let getData = JSON.parse(localStorage.getItem("add-to-cart")) || [];
 
-let wishData = JSON.parse(localStorage.getItem("wishlist"))||[];
+let wishData = JSON.parse(localStorage.getItem("wishlist")) || [];
 
 
-for(let p of getData){
+for (let p of getData) {
     p.Q = 1
 }
 // console.log(getData)
@@ -13,7 +13,7 @@ for(let p of getData){
 var card = document.getElementById("product-container");
 let heading = document.getElementById("heading")
 
-let sum = document .getElementById("sum")
+let sum = document.getElementById("sum")
 
 fetchData(wishData);
 
@@ -22,12 +22,12 @@ fetchData(getData);
 
 
 
-function fetchData(data){
+function fetchData(data) {
 
     card.innerHTML = null;
-    
-    if(getData.length === 0){
-    alert("Empty Cart")
+
+    if (getData.length === 0) {
+        alert("Empty Cart")
     }
 
     let total = 0;
@@ -53,35 +53,35 @@ function fetchData(data){
         description.innerText = ele.description;
 
         let prize = document.createElement("h3");
-        prize.innerText = "Price : "+ele.price+".00 ₹";
-       
+        prize.innerText = "Price : " + ele.price + ".00 ₹";
+
         let removeBtn = document.createElement("button");
         removeBtn.innerText = `Remove ~`;
-        removeBtn.setAttribute("id","btn")
-        removeBtn.addEventListener("click",()=>{
+        removeBtn.setAttribute("id", "btn")
+        removeBtn.addEventListener("click", () => {
 
-            data.splice(ele,1);
+            data.splice(ele, 1);
 
             alert("product remove successfully")
 
-            localStorage.setItem("add-to-cart",JSON.stringify(data));
+            localStorage.setItem("add-to-cart", JSON.stringify(data));
 
             fetchData(data);
             console.log(data);
-            
+
         })
 
         let box = document.createElement("div");
         box.innerHTML = ` <i class="fa-solid fa-trash"></i>`;
 
         let shortBtn = document.createElement("button");
-        shortBtn.setAttribute("id","btn1")
+        shortBtn.setAttribute("id", "btn1")
         shortBtn.innerText = "❤️ Move to Shortlist";
 
-        shortBtn.addEventListener("click",()=>{
-            data.splice(ele,1);
+        shortBtn.addEventListener("click", () => {
+            data.splice(ele, 1);
 
-            localStorage.setItem("wishlist",JSON.stringify(data));
+            localStorage.setItem("wishlist", JSON.stringify(data));
 
             fetchData(data);
             console.log(data);
@@ -94,26 +94,39 @@ function fetchData(data){
 
         let select = document.createElement("select")
         select.innerHTML = "quantity"
+        select.setAttribute("id","qty")
 
         let opt1 = document.createElement("option");
-        opt1.innerText = "1";
-        let opt2 = document.createElement("option");
-        opt2.innerText = "2";
-        let opt3 = document.createElement("option");
-        opt3.innerText = "3";
-        let opt4 = document.createElement("option");
-        opt4.innerText = "4";
-        let opt5 = document.createElement("option");
-        opt5.innerText = "5";
+        opt1.innerText = 1;
+        opt1.value = 1;
 
-        select.addEventListener("change",(e)=>{
+        let opt2 = document.createElement("option");
+        opt2.innerText = 2;
+        opt2.value = 2;
+
+        let opt3 = document.createElement("option");
+        opt3.innerText = 3;
+        opt3.value = 3;
+
+        let opt4 = document.createElement("option");
+        opt4.innerText = 4;
+        opt4.value = 4;
+
+        let opt5 = document.createElement("option");
+        opt5.innerText = 5;
+        opt5.value = 5;
+
+        select.addEventListener("change", (e) => {
+            // let qty = document.getElementById("qty").value;
+            let qty = e.target.value;
+            console.log(qty)
             incre = +(e.target.value)
             select.innerText = incre
             ele.Q = incre
-            localStorage.setItem("add-to-cart",JSON.stringify(data));
+            localStorage.setItem("add-to-cart", JSON.stringify(data));
             fetchData(data);
         })
-       
+
         //.....Size Selection...//
         let size = document.createElement("div");
         size.setAttribute("id", "size");
@@ -133,25 +146,25 @@ function fetchData(data){
         let s5 = document.createElement("option");
         s5.innerText = "XXL";
 
-        localStorage.setItem("wishlist",JSON.stringify(data));
+        localStorage.setItem("wishlist", JSON.stringify(data));
 
-        localStorage.setItem("add-to-cart",JSON.stringify(data));
+        localStorage.setItem("add-to-cart", JSON.stringify(data));
 
         div2.append(img)
         removeBtn.append(box);
         sel.append(select);
-        select.append(opt1,opt2,opt3,opt4,opt5);
+        select.append(opt1, opt2, opt3, opt4, opt5);
         size.append(siz)
-        siz.append(s1,s2,s3,s4,s5)
-        div1.append(title,description,prize,sel,size,removeBtn,shortBtn)
-        div.append(div2,div1);
+        siz.append(s1, s2, s3, s4, s5)
+        div1.append(title, description, prize, sel, size, removeBtn, shortBtn)
+        div.append(div2, div1);
         card.append(div);
-     
-        total = total+ele.price*ele.Q
-       
+
+        total = total + ele.price * ele.Q
+
     })
-    
+
     heading.innerText = total
 
-    
+
 }
